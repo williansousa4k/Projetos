00f08d856ecf19c630597564013ee983ac356812
williansousa4k <williansousa4k@gmail.com>
2023-10-21 07:27:54 -0300

    Primeiro commit

---
+++ andarfor.py
@@ -0,0 +1,6 @@
+i = 0 
+for i in range(20, 0, -1):
+    i = i + 0
+    if i == 13:
+        continue
+    print(i)
---
+++ andrawhile.py
@@ -0,0 +1,6 @@
+i = 21
+while i > 1:
+    i -= 1
+    if i == 13:
+        continue
+    print(i)
